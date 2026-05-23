7776cb34129a2bcc3175dd344fcd2441c14fda69
peter4tech <peterbozz1996@gmail.com>
2026-05-23 03:30:54 +0100

    frontend |backend integration

--- src/ClaudyGod.API/Controllers/TicketController.cs
+++ src/ClaudyGod.API/Controllers/TicketController.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using ClaudyGod.Application.Common.Models;
 using ClaudyGod.Application.Features.Tickets.Commands;
 using ClaudyGod.Application.Features.Tickets.DTOs;
--- src/ClaudyGod.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/ClaudyGod.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -26,14 +26,7 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     public DbSet<User> Users => Set<User>();
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
     public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
-    public DbSet<Testimonial> Testimonials => Set<Testimonial>();
-    public DbSet<LeadershipMember> LeadershipMembers => Set<LeadershipMember>();
     public DbSet<Reel> Reels => Set<Reel>();
-    public DbSet<GivingOption> GivingOptions => Set<GivingOption>();
-    public DbSet<GivingIntent> GivingIntents => Set<GivingIntent>();
-    public DbSet<ContentItem> ContentItems => Set<ContentItem>();
-    public DbSet<WorkforceApplication> WorkforceApplications => Set<WorkforceApplication>();
-    public DbSet<PastoralCareRequest> PastoralCareRequests => Set<PastoralCareRequest>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -51,14 +44,7 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
         modelBuilder.Entity<Event>().HasQueryFilter(e => !e.IsDeleted);
         modelBuilder.Entity<TicketReservation>().HasQueryFilter(e => !e.IsDeleted);
         modelBuilder.Entity<User>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<Testimonial>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<LeadershipMember>().HasQueryFilter(e => !e.IsDeleted);
         modelBuilder.Entity<Reel>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<GivingOption>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<GivingIntent>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<ContentItem>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<WorkforceApplication>().HasQueryFilter(e => !e.IsDeleted);
-        modelBuilder.Entity<PastoralCareRequest>().HasQueryFilter(e => !e.IsDeleted);
     }
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
