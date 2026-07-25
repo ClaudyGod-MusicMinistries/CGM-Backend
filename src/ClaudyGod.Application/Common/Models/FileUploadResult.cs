bf14771c79e420942fcbafaae6fecc8ac4ecf71a
peter4tech <34604032+peterchims@users.noreply.github.com>
2026-07-25 04:59:41 +0100

    fixcees

--- src/ClaudyGod.Application/Common/Models/FileUploadResult.cs
+++
@@ -1,9 +0,0 @@
-namespace ClaudyGod.Application.Common.Models;
-
-public record FileUploadResult(
-    string RelativePath,
-    string FileName,
-    string ContentType,
-    long SizeBytes,
-    string PublicUrl
-);
